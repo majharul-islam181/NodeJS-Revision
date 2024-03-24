2defc0abffba98ac5a195416d427b71c7e4e50ca
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2024-03-24 16:46:00 +0600

    modules

---
+++ hello.js
@@ -0,0 +1,5 @@
+const math = require("./math")
+//module means separation with function Like custom widget flutter
+console.log(math.add(1,2));
+console.log(math.sub(1,2));
+console.log("Hello there");
